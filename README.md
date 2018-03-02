e4fe546379a46f0c4ba2378ef598052c170b2da7
gaiqs <gaiqisong@auto-link.com.cn>
2018-03-02 16:25:27 +0800

    Fist commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WHAT
+小米路由器 shadowsocks 插件
+# USAGE
+cd /tmp && rm -rf *.sh && curl https://raw.githubusercontent.com/Hank-Zhu/miwifi-ss/master/miwifi.sh -o miwifi.sh && chmod +x miwifi.sh && sh ./miwifi.sh && rm -rf *.sh
---
+++ r3/r3_uninstall.sh
@@ -0,0 +1,15 @@
+        echo "开始卸载Shadowsocks"
+        echo -e "停止相关进程--------------------------------------\c"
+        /etc/init.d/shadowsocks stop 1>/dev/null 2>&1
+        /etc/init.d/shadowsocks disable 1>/dev/null 2>&1
+        echo -e "[\e[32m完成\e[37m]"
+        echo -e "删除相关文件--------------------------------------\c"
+        rm -rf /etc/init.d/shadowsocks
+        rm -rf /etc/dnsmasq.d/dnsmasq_list.conf
+	rm -rf /data/usr/sbin
+	rm -rf /etc/shadowsocks.json
+        sed -i '/ipset -N gfwlist iphash -!/d' /etc/firewall.user
+        sed -i '/iptables -t nat -A PREROUTING -p tcp -m set --match-set gfwlist dst -j REDIRECT --to-port 1081/d' /etc/firewall.user
+	/etc/init.d/firewall restart
+	/etc/init.d/dnsmasq restart
+	echo "卸载Shadowsocks完成"
